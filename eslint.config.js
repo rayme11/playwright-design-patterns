e5c8edb20b3b85fd61d050b8ce2155b9c2ee57ca
Ray Maldonado <vivachihuahua2004@gmail.com>
2026-04-29 19:34:37 -0500

    chore: add ESLint 9 config and dependencies for CI lint job

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      // Add or override rules here
+    },
+  },
+];
